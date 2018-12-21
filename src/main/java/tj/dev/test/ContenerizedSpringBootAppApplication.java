c53daa019c6ccbbe4f9e951f045f459c3d73859a
Tomasz Jarych <tjarych@gmail.com>
2018-12-21 14:01:45 +0100

    Config application

---
+++ src/main/java/tj/dev/test/ContenerizedSpringBootAppApplication.java
@@ -0,0 +1,14 @@
+package tj.dev.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContenerizedSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContenerizedSpringBootAppApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/tj/dev/test/ContenerizedSpringBootAppApplicationTests.java
@@ -0,0 +1,17 @@
+package tj.dev.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContenerizedSpringBootAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
