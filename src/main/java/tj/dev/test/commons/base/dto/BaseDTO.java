4da775952f31f4956f0a0c608556c46d3188b8b1
Tomasz Jarych <tjarych@gmail.com>
2019-01-01 19:38:13 +0100

    UserDto added Builder pattern

--- src/main/java/tj/dev/test/commons/base/dto/BaseDTO.java
+++ src/main/java/tj/dev/test/commons/base/dto/BaseDTO.java
@@ -17,7 +17,6 @@ public abstract class BaseDTO {
 	}
 
 	public BaseDTO() {
-		super();
 	}
 
 }
--- src/main/java/tj/dev/test/user/dto/UserDto.java
+++ src/main/java/tj/dev/test/user/dto/UserDto.java
@@ -9,7 +9,7 @@ public class UserDto extends BaseDTO {
 	private String email;
 	private String login;
 
-	public UserDto(Long id, String firstName, String lastName, String email, String login) {
+	private UserDto(Long id, String firstName, String lastName, String email, String login) {
 		super(id);
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -18,7 +18,6 @@ public class UserDto extends BaseDTO {
 	}
 
 	public UserDto() {
-		super();
 	}
 
 	public String getFirstName() {
@@ -51,6 +50,57 @@ public class UserDto extends BaseDTO {
 
 	public void setLogin(String login) {
 		this.login = login;
+	}
+
+	public static class UserDtoBuilder {
+
+		private Long id;
+		private String firstName;
+		private String lastName;
+		private String email;
+		private String login;
+		
+		public UserDtoBuilder() {
+		}
+
+		public UserDtoBuilder setId(Long id) {
+			this.id = id;
+			return this;
+		}
+
+		public UserDtoBuilder setFirstName(String firstName) {
+			this.firstName = firstName;
+			return this;
+		}
+
+		public UserDtoBuilder setLastName(String lastName) {
+			this.lastName = lastName;
+			return this;
+		}
+
+		public UserDtoBuilder setEmail(String email) {
+			this.email = email;
+			return this;
+		}
+
+		public UserDtoBuilder setLogin(String login) {
+			this.login = login;
+			return this;
+		}
+
+		public UserDto build() {
+			return new UserDto(id, firstName, lastName, email, login);
+		}
+
+	}
+
+	public static void main(String[] args) {
+		UserDto dto = new UserDto.UserDtoBuilder().setId(1L)
+				.setFirstName("Jan")
+				.setLastName("Kowalski")
+				.setEmail("jankowal@wp.pl")
+				.setLogin("jank")
+				.build();
 	}
 
 }
